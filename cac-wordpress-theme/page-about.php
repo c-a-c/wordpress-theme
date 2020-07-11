a64473d331c49f0c5428d486d46e4f8a13f28cb0
chirhata <flaghatareal@gmail.com>
2020-07-11 14:15:37 +0900

    スマホ対応できるように修正

--- cac-wordpress-theme/page-about.php
+++ cac-wordpress-theme/page-about.php
@@ -38,7 +38,7 @@ Version     ： 1.0.0
                 弊団体について
             </div>
             <div class="subPageSentence" style="color: #545454;">
-                <p style="margin-top: 50px; width: 1000px; text-align: justify; margin-left: calc(50% - 500px);">
+                <p style="margin-top: 50px; width: 95%; text-align: justify; margin-left: 1%;">
                     弊団体は、1965年4月に京都産業大学創立とともに創設された歴史ある部活動です。
                     「電子計算機」のプログラミングオペレーションの知識を深め、種々の電子計算機により技術及びさまざまな応用問題を分析、研究すると同時に学生間の親睦及び社会に貢献する人間を形成する事を目的とし、活動しています。
                     また、近年ではイラストやミュージックなどの分野へのアプローチも行なっており多様な創作活動が盛んになっています。
@@ -147,7 +147,7 @@ Version     ： 1.0.0
                             <s>京都産業大学 7号館 721号室</s><br>
                             <a style="color:RED; font-size:11px">2017年度秋学期より移転工事のため現在は所在地はありません。</a>
                         </div>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1372.9636644912239!2d135.75846449493372!3d35.06982978887928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001a7dd7f2840af%3A0x7abbf5c27c908791!2z44CSNjAzLTgwNDcg5Lqs6YO95bqc5Lqs6YO95biC5YyX5Yy65LiK6LOA6IyC5pys5bGxIO-8l-WPt-mkqA!5e0!3m2!1sja!2sjp!4v1495968576950" width="500" height="530" frameborder="0" style="position: absolute; border:0; top: 70px; left: 0px" allowfullscreen></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1372.9636644912239!2d135.75846449493372!3d35.06982978887928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001a7dd7f2840af%3A0x7abbf5c27c908791!2z44CSNjAzLTgwNDcg5Lqs6YO95bqc5Lqs6YO95biC5YyX5Yy65LiK6LOA6IyC5pys5bGxIO-8l-WPt-mkqA!5e0!3m2!1sja!2sjp!4v1495968576950" width="90%" height="50%" frameborder="0" style="position: absolute; border:0; top: 130px; left: 0px" allowfullscreen></iframe>
                         <div class="aboutProfile__information--separator"></div>
                     </div>
                 </div>
